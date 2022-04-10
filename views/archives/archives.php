424beb83207fe523e75c8d216e30342d0e91bab4
Mominul Islam <70751323+Levarenox@users.noreply.github.com>
2022-04-10 10:57:20 +0600

    add archive data

--- views/archives/archives.php
+++ views/archives/archives.php
@@ -1,3 +1,28 @@
+<?php
+include('../../connection/db_connect.php');
+
+try {
+  $sql = "SELECT * FROM student_archive";
+
+  $stm = $db->prepare($sql);
+  $stm->execute();
+  $archives = $stm->fetchAll();
+  $stm->closeCursor();
+
+  $sql = "SELECT id, first_name, last_name, country, university_name FROM student_reg";
+
+  $stm = $db->prepare($sql);
+  $stm->execute();
+  $students = $stm->fetchAll();
+  $stm->closeCursor();
+
+  $student_reg_array = array();
+} catch (Exception $e) {
+  echo $e->getMessage();
+}
+?>
+
+
 <!-- Header -->
 <!DOCTYPE html>
 <html lang="en">
@@ -73,13 +98,86 @@
         <div class="main">
 
           <div class="main__search">
-
             <div class="search">
               <span class="search__icon"><i class="fa fa-search" aria-hidden="true"></i></span>
               <input class="search__box" type="text" name="">
             </div>
-
           </div>
+
+          <div style="color: black;">
+            <?php if (isset($archives)) {  ?>
+
+            <?php foreach ($archives as $archive_cell) { ?>
+
+            <div class="archive-box">
+
+              <table class="archive-box__id">
+                <?php foreach ($students as $student_cell) {  ?>
+                <?php if ($archive_cell['student_reg_id'] == $student_cell['id']) { ?>
+                <tr>
+                  <td>Name</td>
+                  <td><?php echo $student_cell['first_name'] . " " . $student_cell['last_name'] ?></td>
+                </tr>
+                <tr>
+                  <td>Country</td>
+                  <td><?php echo $student_cell['country'] ?></td>
+                </tr>
+                <tr>
+                  <td>University Name</td>
+                  <td><?php echo $student_cell['university_name'] ?></td>
+                </tr>
+                <?php } ?>
+                <?php } ?>
+              </table>
+
+              <table class="archive-box__details">
+                <tr>
+                  <th>Company Name</th>
+                  <td id="company-name"><?php echo $archive_cell['company_name'] ?></td>
+                </tr>
+                <tr>
+                  <th>Address</th>
+                  <td id="company-address"><?php echo $archive_cell['company_address'] ?></td>
+                </tr>
+                <tr>
+                  <th>City</th>
+                  <td id="company-city"><?php echo $archive_cell['company_city'] ?></td>
+                </tr>
+                <tr>
+                  <th>Country</th>
+                  <td id="company-country"><?php echo $archive_cell['company_country'] ?></td>
+                </tr>
+                <tr>
+                  <th>Department</th>
+                  <td id="company-department"><?php echo $archive_cell['company_department'] ?></td>
+                </tr>
+                <tr>
+                  <th>Position</th>
+                  <td id="company-position"><?php echo $archive_cell['company_position'] ?></td>
+                </tr>
+                <tr>
+                  <th>Required Skills</th>
+                  <td id="required-skills"><?php echo $archive_cell['required_skills'] ?></td>
+                </tr>
+                <tr>
+                  <th>Start Date</th>
+                  <td id="start-date"><?php echo $archive_cell['start_date'] ?></td>
+                </tr>
+                <tr>
+                  <th>End Date</th>
+                  <td id="end-date"><?php echo $archive_cell['end_date'] ?></td>
+                </tr>
+                <tr>
+                  <th>Work Description</th>
+                  <td id="work-description"><?php echo $archive_cell['work_description'] ?></td>
+                </tr>
+              </table>
+            </div>
+
+            <?php } ?>
+            <?php } ?>
+          </div>
+
 
         </div>
       </main>
