3695f21640d1696c2d8e35c4bcdeae0165878c8e
Mominul Islam <70751323+Levarenox@users.noreply.github.com>
2022-04-18 10:02:27 +0600

    add heroku database

--- connection/db_connect.php
+++ connection/db_connect.php
@@ -1,10 +1,17 @@
 <?php
-DEFINE('DB_USER', 'root');
-DEFINE('DB_PASSWORD', '');
 
-$dsn = 'mysql:host=localhost;dbname=cse482_project_db';
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+$dsn = 'mysql:host=$cleardb_server;dbname=$cleardb_db';
 try {
-    $db = new PDO($dsn, DB_USER, DB_PASSWORD);
+    $db = new PDO($dsn, $cleardb_username, $cleardb_password);
 } catch (PDOException $e) {
     $err_msg = $e->getMessage();
     include('db_error.php');
