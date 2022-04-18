0003fa314f7e0c1937649a83f3cce11bba62003e
Mominul Islam <70751323+Levarenox@users.noreply.github.com>
2022-04-18 10:25:47 +0600

    heroku fix variables

--- connection/db_connect.php
+++ connection/db_connect.php
@@ -9,7 +9,7 @@ $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
 
-$dsn = "mysql:host=' . $cleardb_server . ';dbname=' . $cleardb_db . '";
+$dsn = "mysql:host='$cleardb_server';dbname='$cleardb_db'";
 try {
     $db = new PDO($dsn, $cleardb_username, $cleardb_password);
 } catch (PDOException $e) {
